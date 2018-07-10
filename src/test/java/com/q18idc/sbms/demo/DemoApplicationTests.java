a5f9bf96097a871475a5091571d5811d32619e54
q18idc <993143799@qq.com>
2018-07-11 01:12:10 +0800

    测试类添加生成密码

--- src/test/java/com/q18idc/sbms/demo/DemoApplicationTests.java
+++ src/test/java/com/q18idc/sbms/demo/DemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.q18idc.sbms.demo;
 
+import org.apache.shiro.crypto.hash.SimpleHash;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,7 +11,16 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class DemoApplicationTests {
 
     @Test
-    public void contextLoads() {
+    public void password() {
+        //所需加密的参数  即  密码
+        String source = "123456";
+        //[盐] 一般为用户名 或 随机数
+        String salt = "q18idc.com";
+        //加密次数
+        int hashIterations = 1024;
+
+        SimpleHash hash = new SimpleHash("MD5", source, salt, hashIterations);
+        System.err.println(hash);
     }
 
 }
